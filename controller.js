23cfd7ffa1f3e31e184f0cc761867da05c8ce82c
Wesley Scholl <clients@konjoweb.com>
2021-08-31 16:19:59 -0400

    able to trigger validation, returns empty array

--- controller.js
+++ controller.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const node = require("node")
-const validate = require('health-cards-validation-sdk/js/src/api.js')
+const { validate } = require('health-cards-validation-sdk/js/src/api.js')
 
 // Then import src/api.js and call the right validate.<artifact-type> method, where <artifact-type> can be one of qrnumeric, 
 // healthcard, fhirhealthcard, jws, jwspayload, fhirbundle, or keyset. The validation results, if any, are returned in 
 // Promise-wrapped array. For example you could check a JWS via:
 
-// 
-
 
 router.post("/", function (req, res) {
-    console.log(req.body)
-    const shcData = req.body;
-    const results = validate.shc(shcData);
-    results.then(console.log)
+    console.log(req.body.data)
+    const shcData = req.body.data;
+    const results = validate.qrnumeric(shcData);
+    setTimeout(() => {
+        console.log(results);
+    }, 5000);
 })
 
 module.exports = router;
