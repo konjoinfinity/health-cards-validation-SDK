edd68fa85d43463e52d85f79c6497f7f0151ea64
Wesley Scholl <clients@konjoweb.com>
2021-09-01 14:40:08 -0400

    cleaned index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 app.use(methodOverride("_method"));
-// app.use(passport.initialize());
 
 app.use("/", controller);
 
@@ -21,8 +20,4 @@ app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
     console.log(`Running on PORT: ${app.get("port")}`);
 });
-
-// var server = app.listen(3000, '127.0.0.1',onServerListening);
-
-// app.use('/test', router)
 
