2f69a7c8bb4db221ca949439c04daade173bda10
Konjo Infinity <clients@konjoweb.com>
2021-09-10 15:54:49 -0400

    testing build

--- controller.js
+++ controller.js
@@ -8,17 +8,12 @@ const { validate } = require('./health-cards-validation-sdk/js/src/api')
 // }
 
 router.post("/", async (req, res) => {
-    if (req.body.data == undefined) {
-      console.log("No data posted")
-      res.status(400).json({data: "No data posted"})
-    } else {
       console.log(req.body.data)
     const results = await validate.qrnumeric(req.body.data)
     console.log(results)
     var validNotValid;
       results.length === 0 ? validNotValid = true : validNotValid = false
       res.status(200).json({ data: validNotValid })
-    }
 })
 
 module.exports = router;
