a10acb0dd7445b2b61357152e96e710d946e0518
Wesley Scholl <clients@konjoweb.com>
2021-09-18 10:36:18 -0400

    added error message in response

--- controller.js
+++ controller.js
@@ -13,7 +13,7 @@ router.post("/", async (req, res) => {
     console.log(results)
     var validNotValid;
     results.length === 0 ? validNotValid = true : validNotValid = false
-    res.status(200).json({ data: validNotValid })
+    res.status(200).json({ data: validNotValid, message: validNotValid })
 })
 
 module.exports = router;
