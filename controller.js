45f0bbaecd97f836f5ae3d77f99970ad4d8be848
Wesley Scholl <clients@konjoweb.com>
2021-09-23 12:17:18 -0400

    updated api to accept qr data without an array

--- controller.js
+++ controller.js
@@ -3,13 +3,10 @@ const express = require("express");
 const router = express.Router();
 const { validate } = require('./health-cards-validation-sdk/js/src/api')
 
-// async function thisQR(shc){
-//     return await validate.qrnumeric(shc)
-// }
 
 router.post("/", async (req, res) => {
     console.log(req.body.data)
-    const results = await validate.qrnumeric(req.body.data)
+    const results = await validate.qrnumeric([req.body.data])
     console.log(results)
     var validNotValid;
     results.length === 0 ? validNotValid = true : validNotValid = false
