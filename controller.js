aa0c29879a1d1868a43f6f41e2f4c7ed29b259e2
Wesley Scholl <clients@konjoweb.com>
2021-09-30 08:46:17 -0400

    removed crypto route

--- controller.js
+++ controller.js
@@ -2,9 +2,6 @@ const { checkDataType } = require("ajv/dist/compile/validate/dataType");
 const express = require("express");
 const router = express.Router();
 const { validate } = require('./health-cards-validation-sdk/js/src/api')
-const axios = require('axios');
-const url = 'https://api.probit.com/api/exchange/v1/ticker?market_ids=BITG-USDT,BITG-BTC';
-const options = { headers: { Accept: 'application/json' } };
 
 
 router.post("/", async (req, res) => {
@@ -14,14 +11,6 @@ router.post("/", async (req, res) => {
     var validNotValid;
     results.length === 0 ? validNotValid = true : validNotValid = false
     res.status(200).json({ data: validNotValid, message: results })
-})
-
-router.get("/bitg", async (req, res) => {
-    var price = 0
-    await axios.get(url, options)
-        .then(json => price = json.data.data[0].last)
-    console.log(price)
-    res.status(200).json({ data: price })
 })
 
 module.exports = router;
